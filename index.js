b67a28c77081623d2c8b3c434f5f3d40d3d36905
Ari <luangamath.ari@gmail.com>
2018-08-29 01:02:34 +1000

    Rearranged file structure + added async methods

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-const { Client } = require('discord.js');
-const { token } = require('./settings');
-const client = new Client();
+const Discord = require('discord.js');
+const config  = require('./src/config.json');
+const { token } = require('./src/token.json');
+const bot = new Discord.Client();
+const fs = require('fs'); 
 
-client.on('ready', () => console.log('Ready!'));
-
-
-client.on('message', (msg) => {
-    if (msg.author.bot) return;
-
-    if (msg.content.startsWith('!ping')) {
-        msg.channel.send('!gnip');
-    }
+/**
+ * Bot's awake method, used to intialise and establish bot's current settings 
+ * @param  {[type]}
+ * @param  {[type]}
+ * @return {[type]}
+ */
+bot.on('ready', async () => {
+	console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+	bot.user.setActivity(config.activity, {type: 'STREAMING'});
 });
 
-client.login(token);
+
+/**
+ * Bot's event handler for listening for incoming messages
+ * @param  {[type]}
+ * @param  {[type]}
+ * @return {[type]}
+ */
+bot.on('message', async (msg) => {
+	// prevent botception 
+    if (!msg.content.startsWith(config.prefix) || msg.author.bot) return;
+
+	try {
+		if (msg.content.startsWith(config.prefix + 'ping')) {
+        	msg.channel.send('!gnip');
+    	} 
+	} catch (e) {
+		console.error(e);
+	}
+
+
+});
+
+/**
+ * Initialise bot 
+ */
+bot.login(token);
