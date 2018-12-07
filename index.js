3c0afa85def6dc528c79aa5bcc7e6a2f833060bf
Ari <luangamath.ari@gmail.com>
2018-12-08 02:53:40 +1000

    Implemented user command timeout

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ var mongoDBUrl = process.env.MONGOLAB_URI;
 
 const usr = process.env.MONGOLAB_USER
 const pwd = process.env.MONGOLAB_PASS
+// user state data
+let currUserTime = {}; 
+let prevUserTime = {};
 
 /**
  * Establish mongodb server
@@ -60,10 +63,20 @@ bot.on('message', async (msg) => {
     // Define args
     const args = msg.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
-
+    const now = new Date(); 
 	try {
 		let cmdFile = require(`./src/commands/${cmd}.js`);
-		cmdFile.run(bot, msg, args);
+		currUserTime[msg.author.id] = now;
+		if (prevUserTime[msg.author.id] == null) prevUserTime[msg.author.id] = 0;
+		console.log(prevUserTime[msg.author.id]);
+
+		if (currUserTime[msg.author.id] - prevUserTime[msg.author.id] > Math.round(0.165 * 60 * 1000)) { // if its been more than 10 seconds
+			cmdFile.run(bot, msg, args);
+			prevUserTime[msg.author.id] = now;
+		} else {
+			msg.channel.send(`${msg.author.toString()}, you may not use the \`${cmd}\` command for another `+
+				`\`${+(Math.round(((Math.round(0.165 * 60 * 1000) - (currUserTime[msg.author.id] - prevUserTime[msg.author.id])) / 1000) + 'e+2') + 'e-2')}\` seconds `);
+		}
 	} catch (e) {
 		console.error(e);
 		msg.channel.send(`:x: \`${cmd}\` is an invalid command.`).catch(console.error);
