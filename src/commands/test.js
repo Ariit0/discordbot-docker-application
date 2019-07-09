5f930be8a4f4819932b50aa9118ded9ae70ac3d9
Ari <luangamath.ari@gmail.com>
2019-07-10 00:50:27 +1000

    (WIP): added job query

--- src/commands/test.js
+++ src/commands/test.js
@@ -8,19 +8,51 @@ module.exports = {
 	async execute (msg, args) {
 
         if (typeof args !== 'undefined' && args.length > 0) {
-            var arg1 = args[0];
+            var arg1 = SearchByJobName(jobDB, args[0]);
+
+            if (!arg1) return; // TODO: provide response for invalid query
+            
+            var jobName = PickByJobDetail(jobDB[arg1], 'jobName[A-Za-z]*')
             var jobStats = PickByJobDetail(jobDB[arg1], 'jobStat[A-Za-z]*');
-            var jobDmg = PickByJobDetail(jobDB[arg1], 'jobDmg[A-Za-z]*')
+            var jobDmg = PickByJobDetail(jobDB[arg1], 'jobDmg[A-Za-z]*');
+            console.log(jobName);
             console.log(jobStats);
             console.log(jobDmg);
         }
     }   
 }
 
+/**
+ * 
+ * @param {*} obj 
+ * @param {*} rgx 
+ */
 const PickByJobDetail = (obj, rgx) => {
-    const regex = RegExp(rgx);
+    const regex = new RegExp(rgx);
     var keys = Object.keys(obj).filter(stat => regex.test(stat) === true)
 
     return pick(obj, keys);
+}
+
+/**
+ * 
+ * @param {*} obj 
+ * @param {*} input 
+ */
+const SearchByJobName = (obj, input) => {
+    // match exact input
+    try {
+        // sanitises user input
+        const regex = new RegExp('\\b'+ input.replace(/[-\[\]]+/g, '').toLowerCase() + '\\b');
+
+        var jobName = Object.keys(obj).filter(job => { 
+            // removes numbers, hyphen and brackets in id
+            return regex.test(job.replace(/[0-9]*[-\[\]/(/)]+/g, '')) === true
+        });
+       
+        return (jobName[0].length !== 0 ? jobName[0] : null); 
+    } catch (e) {
+        console.error(e)
+    }
 }
 
