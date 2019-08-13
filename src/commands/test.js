24fa75c7420d9ff165cd395e9b0672f972fffb8b
ari <luangamath.ari@gmail.com>
2019-08-14 01:40:03 +1000

    (WIP): updated db + added more fields to embedded message

--- src/commands/test.js
+++ src/commands/test.js
@@ -8,44 +8,81 @@ module.exports = {
 	async execute (msg, args) {
 
         if (typeof args !== 'undefined' && args.length > 0) {
-            var arg1 = SearchByJobName(jobDB, args[0]);
+            let arg1 = SearchByJobName(jobDB, args[0]);
 
             if (!arg1) return; // TODO: provide response for invalid query
             
-            var { jobName } = PickByJobDetail(jobDB[arg1], 'jobName[A-Za-z]*')
-            var jobStats = PickByJobDetail(jobDB[arg1], 'jobStat[A-Za-z]*');
-            var jobDmg = PickByJobDetail(jobDB[arg1], 'jobDmg[A-Za-z]*');
-            var jobBrk = PickByJobDetail(jobDB[arg1], 'jobBrk[A-Za-z]*');
+            let { jobName } = PickByJobDetail(jobDB[arg1], 'jobName[A-Za-z]*');
+            let { jobType } = PickByJobDetail(jobDB[arg1], 'jobType[A-Za-z]*');
+            let { jobMpRole } = PickByJobDetail(jobDB[arg1], 'jobMpRole[A-Za-z]*');
+            let jobStats = PickByJobDetail(jobDB[arg1], 'jobStat[A-Za-z]*');
+            let jobDmg = PickByJobDetail(jobDB[arg1], 'jobDmg[A-Za-z]*');
+            let jobBrk = PickByJobDetail(jobDB[arg1], 'jobBrk[A-Za-z]*');
+            let jobUlt = PickByJobDetail(jobDB[arg1], 'jobUltimate[A-Za-z]*');
+            let { jobUrlIcon }  = PickByJobDetail(jobDB[arg1], 'jobUrlIcon[A-Z-a-z]*');
 
-            console.log(jobName);
-            console.log(jobStats);
-            console.log(jobDmg);
-            console.log(jobBrk);
-
-            msg.channel.send({embed: {
-                "thumbnail": {
-                    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
-                },
-                "description": "orbs n stuff",
-                "author": {
-                    "name": jobName,
-                    "url": "https://discordapp.com", // redirect to web app
-                    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png" // job type icon
-                },
-                "footer": {
-                    "icon_url": msg.author.avatarURL,
-                    "text": `${msg.author.username}'s request`
-                }
-
-                
+            msg.channel.send({
+                embed: {
+                    "thumbnail": {
+                        "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                    },
+                    "description": `**${jobType}** - ${jobMpRole} : `,
+                    "author": {
+                        "name": jobName,
+                        "url": "https://discordapp.com", // redirect to web app
+                        "icon_url": jobUrlIcon // job type icon
+                    },
+                    "fields": [
+                        {
+                            "name": "**Stats**",
+                            "value": (PrintValue(jobStats) || "blank"),
+                            "inline": true
+                        },
+                        {
+                            "name": "**Ultimate**",
+                            "value": (PrintValue(jobUlt) || "blank"),
+                            "inline": true
+                        },
+                        {
+                            "name": "**Damage Autoes**",
+                            "value": (PrintValue(jobDmg) || "blank"),
+                            "inline": true
+                        },
+                        {
+                            "name": "**Break Autoes**",
+                            "value": (PrintValue(jobBrk) || "blank"),
+                            "inline": true
+                        }
+                    ],
+                    "footer": {
+                        "icon_url": msg.author.avatarURL,
+                        "text": `${msg.author.username}'s request`
+                    }                
                 }
             });
         }
     }   
 }
 
-const EmbededMessage = {
+/**
+ * 
+ * @param {*} data 
+ */
+const PrintValue = (data) => {
+	var string = "";
 
+	for (var key in data) {
+		keyString = key.replace(/job/g, '')
+			.replace(/Stat/g, '')
+			.replace(/Dmg/g, '')
+            .replace(/Brk/g, '')
+            .replace(/Ultimate/g, '')
+			.replace(/\b\w/g, l => l.toUpperCase())
+			.replace(/-/g, ' ');
+
+		string += `**${keyString}**:\t${data[key]}\n`;
+	}
+	return string;
 }
 
 /**
